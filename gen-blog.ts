5501f2bbf116fa3c7f9f6fe52c5774d18318aab3
harumaxy <harumaxy@gmail.com>
2022-05-28 14:03:48 +0900

    add textlint

--- gen-blog.ts
+++ gen-blog.ts
@@ -1,7 +1,7 @@
 const title = Deno.args.at(0) ?? "title";
 const now = new Date();
 const date = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
-const time = `${now.getHours()}:${now.getMinutes()}`;
+const time = `${now.getHours()}:${String(now.getMinutes()).padStart(2, "0")}`;
 
 const filename = `${date}T${time}_${title}.md`;
 
